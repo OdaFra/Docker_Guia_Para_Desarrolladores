446eddb0fd1f3de434a0fd3320d56708a61bd0b5
Oscar Ramirez <osramirezf@gmail.com>
2023-04-06 20:48:02 -0400

    fix: Actualizamos los archivos app.js y el dockerfile.

--- docker_file/cron_ticket/app.js
+++ docker_file/cron_ticket/app.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 let time = 0;
 cron.schedule('1-59/5 * * * * *', () => {
   time++;
-  console.log('Tick cada 5 segundos', time);
+  console.log('Tick cada 5 segundos: ', time);
 });
 
 console.log('Inicio');
