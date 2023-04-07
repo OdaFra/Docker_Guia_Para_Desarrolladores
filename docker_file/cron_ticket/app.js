b2c1b43fdde5b89e7f5e98ef6cdf97292be06d48
Oscar Ramirez <osramirezf@gmail.com>
2023-04-06 20:05:08 -0400

    feat: Construccion de un docker file utilizando image base de node sobre alpine.

---
+++ docker_file/cron_ticket/app.js
@@ -0,0 +1,9 @@
+const cron = require('node-cron');
+
+let time = 0;
+cron.schedule('1-59/5 * * * * *', () => {
+  time++;
+  console.log('Tick cada 5 segundos', time);
+});
+
+console.log('Inicio');
