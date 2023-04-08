40d7508d5929bfdf03aeca58779060daa99d850f
Oscar Ramirez <osramirezf@gmail.com>
2023-04-08 16:34:30 -0400

    fix: Actualizamos la funcion del cron agregando la funcion syncDB centralizada en la carpeta tasks.

--- docker_file/cron_ticket/app.js
+++ docker_file/cron_ticket/app.js
@@ -1,9 +1,6 @@
 const cron = require('node-cron');
+const { syncDB } = require('./tasks/sync-db');
 
-let time = 0;
-cron.schedule('1-59/5 * * * * *', () => {
-  time++;
-  console.log('Tick cada 5 segundos: ', time);
-});
+console.log('Inicio de la app');
 
-console.log('Inicio');
+cron.schedule('1-59/5 * * * * *', syncDB);
